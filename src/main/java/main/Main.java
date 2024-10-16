ffa98a61f2af247094a795561f1460893cd24da3
ivan.tyulkin <ivan.tyulkin@yahoo.com>
2024-10-16 10:17:56 +0300

    Generate

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,10 +1,16 @@
 package main;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+
+/*
+Код генерирует случайные телефонные номера и добавляет их в массив `phones` в структуре `testData`.
+Каждый телефонный номер начинается с "890", и затем добавляются случайные 9 цифр, чтобы получить полный номер.
+Основная функция `generate` запускает несколько потоков, каждый из которых добавляет случайный номер в `testData`.
+
+Этот код можно использовать для тестирования систем, которые требуют случайные телефонные номера, например,
+для создания тестовых данных в приложениях, работающих с контактами или пользователями.
+ */
 
 public class Main {
     public static void main(String[] args) {
